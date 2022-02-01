cb64ce1b49bc70a1bdc1134d335eab876364b8d4
Azat Serikov <azatsdev@gmail.com>
2022-02-01 18:42:05 +0700

    test(verifier): hashing

--- contracts/MessageVerifier.sol
+++ contracts/MessageVerifier.sol
@@ -5,4 +5,12 @@ contract MessageVerifier {
     function pack(string memory _message) public pure returns(bytes memory) {
         return abi.encodePacked(_message);
     }
+
+    function keccak(string memory _message) public pure returns(bytes32) {
+        return keccak256(pack(_message));
+    }
+
+    function sha(string memory _message) public pure returns(bytes32) {
+        return sha256(pack(_message));
+    }
 }
--- test/message-verifier.ts
+++ test/message-verifier.ts
@@ -1,5 +1,9 @@
 import { expect } from "chai";
-import { solidityPack } from "ethers/lib/utils";
+import {
+  solidityKeccak256,
+  solidityPack,
+  soliditySha256,
+} from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { MessageVerifier } from "../typechain";
 
@@ -17,8 +21,25 @@ describe("MessageVerifier", function () {
     const packed = solidityPack(["string"], [message]);
 
     const _packed = await messageVerifier.pack(message);
-    console.log(packed, _packed);
 
     expect(packed).to.equal(_packed);
+  });
+
+  it("Should keccak a message", async () => {
+    const message = "hello";
+    const hash = solidityKeccak256(["string"], [message]);
+
+    const _hash = await messageVerifier.keccak(message);
+
+    expect(hash).to.equal(_hash);
+  });
+
+  it("Should sha a message", async () => {
+    const message = "hello";
+    const hash = soliditySha256(["string"], [message]);
+
+    const _hash = await messageVerifier.sha(message);
+
+    expect(hash).to.equal(_hash);
   });
 });
